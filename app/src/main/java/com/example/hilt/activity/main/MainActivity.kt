da4eb84d3f525664a46653b232cd6159ee645d9c
Ankit Gupta <ankit.gup20@gmail.com>
2021-07-08 11:12:54 +0530

    Basic field and constructor injection done. Test-1 implemented.

--- app/src/main/java/com/example/hilt/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/hilt/activity/main/MainActivity.kt
@@ -9,40 +9,40 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    @Inject
-//    lateinit var human : Human
+    @Inject
+    lateinit var human : Human
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        human.printGenders()
+        human.printGenders()
     }
 }
 
-//class Human @Inject constructor(
-//    private val male : Male,
-//    private val female: Female
-//){
-//    fun printGenders() {
-//        male.gender()
-//        female.gender()
-//    }
-//}
-//
-//class Male @Inject constructor() : Gender{
-//    override fun gender() {
-//        println("I'm a male")
-//    }
-//}
-//
-//class Female @Inject constructor() : Gender{
-//    override fun gender() {
-//        println("I'm a female")
-//    }
-//
-//}
-//
-//interface Gender{
-//    fun gender()
-//}
+class Human @Inject constructor(
+    private val male : Male,
+    private val female: Female
+){
+    fun printGenders() {
+        male.gender()
+        female.gender()
+    }
+}
+
+class Male @Inject constructor() : Gender{
+    override fun gender() {
+        println("I'm a male")
+    }
+}
+
+class Female @Inject constructor() : Gender{
+    override fun gender() {
+        println("I'm a female")
+    }
+
+}
+
+interface Gender{
+    fun gender()
+}
